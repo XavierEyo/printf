1b8a8b06de437f938b849b901e10b08981d89481
XavierEyo <udoeyo91@gmail.com>
2022-08-11 10:29:47 -0700

    syntax interpreter

--- syntax_i.c
+++ syntax_i.c
@@ -96,6 +96,4 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 		ret += _memcpy(output, (format + i), 1);
 		i += (len != 0) ? 1 : 0;
 	}
-	cleanup(args, output);
-	return (ret);
-}
+	return (ret)}
